f2f4d701ada5d26019e832017ed8f2c66543108a
avokeys <alexander.ayala.palacin@gmail.com>
2022-03-14 19:11:40 +0000

    added cards with titles, need to add data

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,43 +1,33 @@
 const getState = ({ getStore, getActions, setStore }) => {
+	const swApi = "https://www.swapi.tech/api";
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
+			characters: [
+						
+			],
+			planets: [
+
+			],
+			vehicles:[
+
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+			
+
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
+				
+					fetch(swApi + "/people").then(
+						res => res.json()
+					).then(data => setStore({ characters: data.results }));
+					fetch(swApi + "/planets").then(
+						res => res.json()
+					).then(data => setStore({ planets: data.results }));
+					fetch(swApi + "/vehicles").then(
+						res => res.json()
+					).then(data => setStore({ vehicles: data.results }));
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,24 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useContext} from "react";
 import "../../styles/home.css";
+import { DataRow } from "../component/dataRow";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
+export const Home = () => {
+	const {store,actions} = useContext(Context);
+	return(
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		{store.characters.length > 0 && (
+					<DataRow title= "Characters" data={store.characters} />
+            )}
+			{store.planets.length > 0 && (
+						<DataRow title= "Planets" data={store.planets}/>
+			)}
+			{store.vehicles.length > 0 && (
+		<DataRow title= "Vehicles" data={store.vehicles}/>
+)}
 	</div>
-);
+	)
+	};
+	
+
+
