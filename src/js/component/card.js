51d3394cdf1a9dcdb4954843bea6ec4d5c59aab2
avokeys <alexander.ayala.palacin@gmail.com>
2022-03-17 19:23:12 +0000

    added cards for planets, vehicles, and characters with names. style d-flex

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,23 +1,22 @@
+import PropTypes from "prop-types";
 import React, { Component } from "react";
 
-export const DataRow = (props) => {
+export const Card  = (props) => {
 	return(
-        <div>
-            <h2>{props.title}
-            </h2>
-            <div className="row">
-                <div className="col">
                     <div className="card" >
                         {/* <img src="..." className="card-img-top" alt="...">  */}
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
+                            <h5 className="card-title">{props.data.name}</h5>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" className="btn btn-primary">Go somewhere</a>
                         </div>
-                    </div>
-                </div>
-            </div>
-        </div>   
+                    </div>              
     )
     
     };
+
+    Card.propTypes ={
+        data : PropTypes.object
+    }
+
+    
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,22 +1,45 @@
 import React, {useContext} from "react";
 import "../../styles/home.css";
-import { DataRow } from "../component/dataRow";
+import { Card } from "../component/card";
 import { Context } from "../store/appContext";
 
 export const Home = () => {
 	const {store,actions} = useContext(Context);
 	return(
-	<div className="text-center mt-5">
-		{store.characters.length > 0 && (
-					<DataRow title= "Characters" data={store.characters} />
-            )}
-			{store.planets.length > 0 && (
-						<DataRow title= "Planets" data={store.planets}/>
-			)}
-			{store.vehicles.length > 0 && (
-		<DataRow title= "Vehicles" data={store.vehicles}/>
-)}
+	<div className="text-center mt-5  ">
+		<h2>Characters</h2>
+		<div className="characters d-flex">
+			{store.characters.length > 0 &&  
+				store.characters.map( ( item,index)=>{
+					return(
+						<Card key={index} data= {item} />
+					)
+				}) 
+			}
+		</div>
+		<h2>Planets</h2>
+		<div className="planets d-flex">
+			{store.planets.length > 0 && 
+			store.planets.map((item,index) =>{
+				return(
+					<Card key={index} data = {item}/>
+				)
+		})}
+
+		</div>
+		<h2>Vehicles</h2>
+		<div className="vehicles d-flex">
+		{store.vehicles.length > 0 && 
+			store.vehicles.map((item,index) =>{
+				return(
+					<Card key={index} data = {item}/>
+				)
+		})}
+		</div>
+		
+						
 	</div>
+	
 	)
 	};
 	
